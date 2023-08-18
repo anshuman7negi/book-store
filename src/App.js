bfcaf309969f29226f23009486edd4f93838ede7
anshuman7negi <anshumannegi777@gmail.com>
2023-08-18 10:07:02 +0530

    Added categories to project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Book from './components/Book';
 import Categories from './components/Categories';
+import Books from './components/Books';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={Book} />
-        <Route path="/categories" element={Categories} />
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </div>
   );
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,10 @@
+import './styles/Category.css';
+
 const categories = () => (
-  <div />
+  <div className="CategoryContainer">
+    <h1>Check your status</h1>
+    <button className="statusButton" type="button">Check Status</button>
+  </div>
 );
 
 export default categories;
